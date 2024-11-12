9a6cfda048fd3fd3a6a5ea9f9db4dd3c5426fd45
james <jamesallynmackinnon@gmail.com>
2024-11-11 23:35:29 -0800

    fixed wallpaper

--- hypr/.config/hypr/scripts/random_wallpaper.sh
+++ hypr/.config/hypr/scripts/random_wallpaper.sh
@@ -3,12 +3,13 @@
 slideshow_dir="$HOME/Downloads/Backgrounds/Slideshow/"
 
 slideshow=$(find "$slideshow_dir" -type f)
+sleep 1s
 
 while true; do
 	wallpaper=$(printf "%s \n" ${slideshow[@]} | shuf -n 1)
 	hyprctl hyprpaper unload all
 	hyprctl hyprpaper preload "$wallpaper"
 	sleep 1s
-	hyprctl hyprpaper wallpaper ,"$wallpaper"
-	sleep 5s
+	hyprctl hyprpaper wallpaper eDP-1, "$wallpaper"
+	sleep 5m
 done
