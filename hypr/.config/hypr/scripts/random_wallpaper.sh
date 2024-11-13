d431767c4dd16806924363b252715c95e1852708
James <=>
2024-11-13 13:08:33 -0800

    idk

--- hypr/.config/hypr/scripts/random_wallpaper.sh
+++ hypr/.config/hypr/scripts/random_wallpaper.sh
@@ -9,7 +9,6 @@ while true; do
 	wallpaper=$(printf "%s \n" ${slideshow[@]} | shuf -n 1)
 	hyprctl hyprpaper unload all
 	hyprctl hyprpaper preload "$wallpaper"
-	sleep 1s
 	hyprctl hyprpaper wallpaper eDP-1, "$wallpaper"
 	sleep 5m
 done
