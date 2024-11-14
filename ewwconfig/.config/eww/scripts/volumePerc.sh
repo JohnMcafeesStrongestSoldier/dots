e0075f4e502aab30fc80ac8fe6bc32609219407c
james <jamesallynmackinnon@gmail.com>
2024-11-13 17:15:44 -0800

    wip

--- ewwconfig/.config/eww/scripts/volumePerc.sh
+++ ewwconfig/.config/eww/scripts/volumePerc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pactl subscribe | while read -r line; do 
-    pactl get-sink-volume 0 | awk '{printf "%s", $5; fflush()}'
-    echo ""
+pactl subscribe | while read -r line; do
+	pactl get-sink-volume 0 | grep "Event \'change\' on sink " | awk '{printf "%s", $3; fflush()}'
+	echo ""
 done
