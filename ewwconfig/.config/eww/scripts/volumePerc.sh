409e2231d06f9228b1ae43a48f6854e0763bf897
James <=>
2024-11-14 22:45:37 -0800

    eww configs

--- ewwconfig/.config/eww/scripts/volumePerc.sh
+++ ewwconfig/.config/eww/scripts/volumePerc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 pactl subscribe | while read -r line; do
-	pactl get-sink-volume 0 | grep "Event \'change\' on sink " | awk '{printf "%s", $3; fflush()}'
-	echo ""
+	# Check if the line indicates a sink volume change event
+	if echo "$line" | grep -q "Event 'change' on sink"; then
+		# Get the current volume of the specified sink
+		pactl get-sink-volume 0 | awk '/Volume:/ {print substr($5, 1, length($5)-1)}'
+	fi
 done
